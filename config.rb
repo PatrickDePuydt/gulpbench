01a437aab18d89d66022d45a95cebadad6dcd912
Patrick DePuydt <prdepuydt@gmail.com>
2015-12-15 15:02:34 -0500

    Grrr

--- config.rb
+++ config.rb
@@ -1,10 +1,13 @@
 project_type = :stand_alone
-http_path = "/"
+http_path = "app/"
 sass_dir = "scss"
-css_dir = "css"
-images_dir = "images"
+sass_path = "/"
+sourcemap = true
+sass_options = {:sourcemap => true}
+css_dir = "app/css"
+images_dir = "app/images"
 fonts_dir = "fonts"
-javascript_dir = "js"
+javascript_dir = "app/js"
 line_comments = false
 preferred_syntax = :scss
 output_style = :compressed
--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,66 @@
-var gulp = require ('gulp'),
+var browserSync     = require('browser-sync').create();
+
+var gulp = require('gulp'),
+	autoprefixer = require('gulp-autoprefixer'),
 	compass = require('gulp-compass'),
 	uglify = require('gulp-uglify'),
+	plumber = require('gulp-plumber'),
 	rename = require('gulp-rename');
 
 
-gulp.task('compass', function(){
-	gulp.src('app/scss/styles.scss')
-	.pipe(compass({
-		config_file: './config.rb',
-		css: 'app/css',
-		sass: 'app.scss'
-	}))
-	.pipe(gulp.dest('app/css/'));
+gulp.task('html', function(){
+	gulp.src('app/**/*.html')
+	.pipe(browserSync.stream());
 });
+
+
 
 gulp.task('scripts', function() {
 	gulp.src(['app/js/**/*.js', '!app/js/**/*/.min.js'])
+	.pipe(plumber())
 	.pipe(rename({suffix:'.min'}))
 	.pipe(uglify())
 	.pipe(gulp.dest('app.js'));
 });
 
-gulp.task('watch', function(){
-	gulp.watch('app/js/**/*.js', ['scripts']);
+gulp.task('compass', function(){
+	gulp.src('app/scss/styles.scss')
+	.pipe(plumber())
+	.pipe(compass({
+		config_file: 'config.rb',
+		sourcemap: true,
+		debug: true,
+		css: 'app/css',
+		sass: 'app/scss'
+	}))
+	.pipe(autoprefixer())
+	.pipe(gulp.dest('app/css/'))
+	.pipe(browserSync.stream());
 });
 
-gulp.task('default', ['scripts', 'watch']);
+//Local Server
+gulp.task('serve', function() {
+
+  browserSync.init({
+
+      server: 'app/'
+
+  });
+
+
+	gulp.watch('app/scss/**/*.scss', ['compass']);
+
+	gulp.watch('app/html/**/*.html', ['html']);
+
+	//Watches for changes to the HTML and then reloads.
+	gulp.watch('app/**/*.html').on('change', browserSync.reload());
+
+});
+
+gulp.task('watch', function(){
+	gulp.watch('app/js/**/*.js', ['scripts']);
+	gulp.watch('app/scss/**/*.scss', ['compass']);
+	gulp.watch('app/html/**/*.html', ['html']);
+});
+
+gulp.task('default', ['scripts', 'html', 'compass', 'watch', 'serve']);
